a611bba0a51b26e9ea76d512c93b0e25f24fe19a
Twilight Owl <zhelnerovichda@gmail.com>
2018-02-14 01:52:39 +0700

    old files and comments are deleted

--- ExpoVersion/app/actions/index.js
+++ ExpoVersion/app/actions/index.js
@@ -2,32 +2,6 @@ import { createAction } from 'redux-act';
 import _ from 'lodash';
 import * as requests from '../services/requests';
 
-export const add = createAction('ADD');
-export const increment = createAction('INC');
-export const decrement = createAction('DEC');
-export const receive = () => 
-	dispatch => {
-		dispatch(increment());
-		return requests.receiveImageByID(5)
-			.then(result => {
-				dispatch(increment());
-				return result;
-			})
-			.catch(error => error);
-	};
-
-
-export const goFirst = createAction('GO_FIRST');
-export const goPrevious = createAction('GO_PREVIOUS');
-export const goPage = createAction('GO_PAGE');
-export const goNext = createAction('GO_NEXT');
-export const goLast = createAction('GO_LAST');
-
-
-//receiveImageByID, receiveImages, receiveImageCount
-export const showImageByID = createAction('SHOW_IMAGE_BY_ID');
-export const closeCurrentImage = createAction('CLOSE_CURRENT_IMAGE');
-
 export const imageCountLoading = createAction('IMAGE_COUNT_LOADING');
 export const imageCountSuccess = createAction('IMAGE_COUNT_SUCCESS');
 export const imageCountError = createAction('IMAGE_COUNT_ERROR');
@@ -38,33 +12,6 @@ export const receiveImageCount = () =>
 			.then(result => dispatch(imageCountSuccess(result)))
 			.catch(error => dispatch(imageCountError(error)));
 	};
-
-/*export const imageLoading = createAction('IMAGE_LOADING');
-export const imageSuccess = createAction('IMAGE_SUCCESS');
-export const imageError = createAction('IMAGE_ERROR');
-export const receiveImageByID = ID =>
-	dispatch => {
-		dispatch(imageLoading());
-		return requests.receiveImageByID(ID)
-			.then(result => {
-				//console.log(result);
-				return dispatch(imageSuccess(result))
-			})
-			.catch(error => dispatch(imageError(error)));
-	};
-
-export const imagesLoading = createAction('IMAGES_LOADING');
-export const imagesSuccess = createAction('IMAGES_SUCCESS');
-export const imagesError = createAction('IMAGES_ERROR');
-export const receiveImages = (offset, limit) =>
-	dispatch => {
-		dispatch(imagesLoading());
-		return requests.receiveImages(offset, limit)
-			.then(result => {
-				return dispatch(imagesSuccess(result))
-			})
-			.catch(error => dispatch(imagesError(error)));
-	};*/
 
 export const imageLoading = createAction('IMAGE_LOADING');
 export const imageSuccess = createAction('IMAGE_SUCCESS');
@@ -81,48 +28,11 @@ export const receiveImagesAsync = (offset, limit) =>
 		)
 	};
 
+export const goFirst = createAction('GO_FIRST');
+export const goPrevious = createAction('GO_PREVIOUS');
+export const goPage = createAction('GO_PAGE');
+export const goNext = createAction('GO_NEXT');
+export const goLast = createAction('GO_LAST');
 
-// export default {
-// 	add: createAction('ADD'),
-// 	increment: createAction('INC'),
-// 	decrement: createAction('DEC'),
-
-// 	receive:
-// }
-
-/*export const receive = (add) => {
-	console.log('AAAAAAAAAAAAAAAAAAA')
-	add();
-	return receiveImageByID(5)
-		.then(result => {
-			add();
-			return result;
-		})
-		.catch(error => error);
-};*/
-
-
-
-
-
-
-/*
-function fetch() {
-  // state: { running: false, result: false }
-  start();
-  // state: { running: true, result: false }
-  return new Promise(resolve => {
-    // Here, you should probably do a real async call,
-    // like, you know, XMLHttpRequest or Global.fetch stuff
-    setTimeout(() =>
-      resolve(1)
-    , 5);
-  }).then(result=>
-    success(result)
-    // state: { running: false, result: 1 }
-  );
-}
-
-fetch().then(() => {
-  // state: { running: false, result: 1 }
-});*/
+export const showImageByID = createAction('SHOW_IMAGE_BY_ID');
+export const closeCurrentImage = createAction('CLOSE_CURRENT_IMAGE');
--- ExpoVersion/app/containers/Container.js
+++ ExpoVersion/app/containers/Container.js
@@ -8,21 +8,18 @@ import ImageLayout from '../components/ImageLayout';
 import PageLayout from '../components/PageLayout';
 import ButtonLayout from '../components/ButtonLayout';
 
-//import { Pages } from 'react-native-pages';
-//import Pager from './Pager';
-
 const styles = StyleSheet.create({
-		container: {
-			flex: 1,
-		},
-		content: {
-			position: 'absolute',
-			top: 20,
-			right: 0,
-			left: 0,
-			bottom: 35,
+        container: {
+            flex: 1,
+        },
+        content: {
+            position: 'absolute',
+            top: 20,
+            right: 0,
+            left: 0,
+            bottom: 35,
             backgroundColor: '#444',
-		},
+        },
         statusBarLayout: {
             position: 'absolute',
             top: 0,
@@ -31,43 +28,43 @@ const styles = StyleSheet.create({
             height: 40,
             backgroundColor: '#444',
         }
-	}),
-	mapStateToProps = state => ({
-		pages: { ...state.pages },
-		images: { ...state.images }
-	}),
-	mapDispatchToProps = dispatch => 
-		Object.keys(actions)
-			.reduce((acc, key) => ({ ...acc, [key]: (...args) => dispatch(actions[key](...args)) }), {});	
+    }),
+    mapStateToProps = state => ({
+        pages: { ...state.pages },
+        images: { ...state.images }
+    }),
+    mapDispatchToProps = dispatch => 
+        Object.keys(actions)
+            .reduce((acc, key) => ({ ...acc, [key]: (...args) => dispatch(actions[key](...args)) }), {});   
 
 class AppComponent extends React.Component {
 
-	constructor(props) {
-		super(props);
-		BackHandler.addEventListener('hardwareBackPress', () => {
-			if (!this.props.images.currentImage) {
-		    	this.props.closeCurrentImage();
-		    	return true;
-		  	}
-		  	return false;
-		});
-	}
+    constructor(props) {
+        super(props);
+        BackHandler.addEventListener('hardwareBackPress', () => {
+            if (!this.props.images.currentImage) {
+                this.props.closeCurrentImage();
+                return true;
+            }
+            return false;
+        });
+    }
 
-	componentDidMount() {
-		this.props.receiveImageCount();
-		this.props.receiveImagesAsync(this.props.pages.offset, this.props.pages.limit);
-	}
+    componentDidMount() {
+        this.props.receiveImageCount();
+        this.props.receiveImagesAsync(this.props.pages.offset, this.props.pages.limit);
+    }
 
-  	componentWillReceiveProps(nextProps) {
-  		if (this.props.pages.offset !== nextProps.pages.offset ||
-  			this.props.pages.limit !== nextProps.pages.limit) {
-  			this.props.receiveImagesAsync(nextProps.pages.offset, nextProps.pages.limit);
-  		}
-  	}
+    componentWillReceiveProps(nextProps) {
+        if (this.props.pages.offset !== nextProps.pages.offset ||
+            this.props.pages.limit !== nextProps.pages.limit) {
+            this.props.receiveImagesAsync(nextProps.pages.offset, nextProps.pages.limit);
+        }
+    }
 
-	render() {
-		const { offset, limit, currentPage, pageCount } = this.props.pages,
-			{ currentImage, loadedImages } = this.props.images;
+    render() {
+        const { offset, limit, currentPage, pageCount } = this.props.pages,
+            { currentImage, loadedImages } = this.props.images;
 
         return <View style={styles.container}>
                 <View style={styles.statusBarLayout}>
@@ -97,183 +94,30 @@ class AppComponent extends React.Component {
                 }
         </View>;
 
-		
-		return <View style={styles.container}>
-			<View style={styles.content}>
-				{currentImage
-					? <ImageLayout width={300} height={300} 
-						URL={loadedImages[currentImage].image.URL} onTap={this.props.closeCurrentImage}/>
-					: <PageLayout rows={5} columns={2} 
-						images={loadedImages} 
-						imagesIDList={_.range(offset, offset + limit)}
-						showImageAction={this.props.showImageByID}
-					/>
-				}
-			</View>
-			<ButtonLayout 
-				currentPage={currentPage} 
-				isFirst={currentPage <= 0}
-				isLast={currentPage >= pageCount - 1}
-				goFirst={this.props.goFirst}
-				goPrevious={this.props.goPrevious}
-				goNext={this.props.goNext}
-				goLast={this.props.goLast}
-			/>
-      	</View>;
-	}
+        
+        return <View style={styles.container}>
+            <View style={styles.content}>
+                {currentImage
+                    ? <ImageLayout width={300} height={300} 
+                        URL={loadedImages[currentImage].image.URL} onTap={this.props.closeCurrentImage}/>
+                    : <PageLayout rows={5} columns={2} 
+                        images={loadedImages} 
+                        imagesIDList={_.range(offset, offset + limit)}
+                        showImageAction={this.props.showImageByID}
+                    />
+                }
+            </View>
+            <ButtonLayout 
+                currentPage={currentPage} 
+                isFirst={currentPage <= 0}
+                isLast={currentPage >= pageCount - 1}
+                goFirst={this.props.goFirst}
+                goPrevious={this.props.goPrevious}
+                goNext={this.props.goNext}
+                goLast={this.props.goLast}
+            />
+        </View>;
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppComponent);
-
-	
-
-	/*componentDidUpdate() {
-    	//console.log('DID UPDATE', this.props.pages.current);
-    	//if (this.viewPager) this.viewPager.setPage(this.props.pages.current);
-    	//if (this.viewPager) this.viewPager.setPage(1);
-  	}*/
-
-	// goToPage(page) {
- 	// this.props.goPage(page); 
- 	// }
-
-/*
-		return (
-	      	<Pager horizontal={true}>
-	      		<Text>1</Text>
-	      		<Text>2</Text>
-	      	</Pager>
-	    );
-
-		return <View style={styles.container}>
-        	<ViewPagerAndroid
-				onPageSelected={e => {
-					console.log('onPageSelected', e.nativeEvent.position);
-					//this.goToPage(e.nativeEvent.position);
-					if (e.nativeEvent.position === 0) this.props.goPrevious(); 
-					else if (e.nativeEvent.position === 2) this.props.goNext(); 
-					//this.props.page.current === 0 ?
-          		}}
-          		onPageScroll={e => {}}
-          		onPageScrollStateChanged={e => {}}
-          		style={styles.viewPager}
-          		//initialPage={this.props.page.current > 0 ? 1 : 0}
-          		initialPage={1}
-          		ref={viewPager => { this.viewPager = viewPager; }} 
-          	>
-          	
-          		{	
-          			this.props.page.currentPage > 0 || true
-          				? <View style={styles.pageStyle} key="previous">
-	            			<Text>Previous Page</Text>
-          				</View>
-          				: null
-          		}
-          		<View style={styles.pageStyle} key="current">
-	            	<Text>Current Page {this.props.page.currentPage}</Text>
-	            	<Image style={{ width: 350, height: 350 }}
-                    	source={{uri: 'http://placehold.it/600/24f355'}}
-                	/>
-          		</View>
-          		{
-          			this.props.page.currentPage < this.props.page.pageCount - 1 || true
-          				? <View style={styles.pageStyle} key="next">
-	            			<Text>Next Page</Text>
-	            			<Image style={{ width: 350, height: 350 }}
-                    			source={{uri: 'http://placehold.it/600/d32776'}}
-                			/>
-          				</View>
-          				: null
-          		}
-        	</ViewPagerAndroid>
-        	<View style={styles.buttonContainer}>
-	          	<Text>{this.props.page.currentPage}</Text>
-          		<Button onPress={this.props.goPrevious} title='<' />
-          		<Text> | </Text>
-          		<Button onPress={this.props.goNext} title='>' />
-        	</View>
-      	</View>;
-
-
-		let data = _.range(200).map(i => ({key: i}));
-		return <ListView
-  				//data={[{key: 'a'}, {key: 'b'}]}
-  				data={data}
-  				renderItem={({item}) => <Text>{item.key}</Text>}
-  				dataSource={data}
-		        renderRow={(rowData) => <Text>{item.key}</Text>}
-  				horizontal={true}
-			/>
-*/
-
-/*
-const styles = StyleSheet.create({
-  viewPager: {
-    flex: 1,
-    backgroundColor: '#efe',
-    padding:20
-    //top: 20,
-    //bottom: 170
-  },
-  pageStyle: {
-    alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#efe',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#444',
-    paddingTop: 20,
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 0,
-    //position: 'relative'
-
-    // paddingTop: 50,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  content: {
-  	position: 'absolute',
-    top: 20,
-    right: 0,
-    left: 0,
-    bottom: 35,
-    //backgroundColor: '#ddf',
-  },
-  buttonContainer: {
-  	position: 'absolute',
-	height: 35,
-    right: 0,
-    left: 0,
-    bottom: 0,
-    flexDirection: 'row',
-    backgroundColor: '#444',
-  },
-  buttonView: {
-  	height: 35,
-  	flex: 2,
-  	marginRight: 1,
-  	marginLeft: 1
-  },
-  button: {
-  	color: '#444'
-  },
-
-  pageNumberView: {
-  	minWidth: 25,
-  	alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  pageNumber: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-    fontSize: 18,
-    color: '#fff'
-  }
-});
-*/
