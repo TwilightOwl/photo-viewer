956350fd74c8274d2730159708de82489e693cb7
Twilight Owl <zhelnerovichda@gmail.com>
2018-02-14 13:51:35 +0700

    update build version

--- BuildVersion/app/reducers/index.js
+++ BuildVersion/app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-//import reducer from './reducer';
 import pageReducer from './pageReducer';
 import imageReducer from './imageReducer';
 
 export default combineReducers({
-  //root: reducer,
-  pages: pageReducer,
-  images: imageReducer
+	pages: pageReducer,
+	images: imageReducer
 });
